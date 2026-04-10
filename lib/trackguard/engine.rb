59a0cfd2ce26f388996aa8248ffcd310cab191b3
Krzysztof Rygielski <krzysztof@rygiel.net>
2026-04-10 12:13:36 +0200

    Auto-include Trackguard::ApplicationHelper in host app (v0.5.1)
    Engine initializer hooks into action_controller load to make
    trackguard_meta_tags available in host app views without any manual include.

--- lib/trackguard/engine.rb
+++ lib/trackguard/engine.rb
@@ -2,6 +2,12 @@ module Trackguard
   class Engine < ::Rails::Engine
     isolate_namespace Trackguard
 
+    initializer "trackguard.helpers" do
+      ActiveSupport::on_load(:action_controller) do
+        helper Trackguard::ApplicationHelper
+      end
+    end
+
     initializer "trackguard.importmap", before: "importmap" do |app|
       if app.config.respond_to?(:importmap)
         app.config.importmap.paths << root.join("config/importmap.rb")
