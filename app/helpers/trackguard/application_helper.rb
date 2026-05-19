2d4364bdc346ab2493fa273909fe818d54bb1ffd
Krzysztof Rygielski <krzysztof@rygiel.net>
2026-05-20 00:24:13 +0200

    Add trackguard_hub_js_tag helper for cloud adapter JS injection

--- app/helpers/trackguard/application_helper.rb
+++ app/helpers/trackguard/application_helper.rb
@@ -1,5 +1,11 @@
 module Trackguard
   module ApplicationHelper
+    def trackguard_hub_js_tag
+      return unless Trackguard.adapter.is_a?(Trackguard::Adapters::Hub)
+
+      tag.script(src: "https://app.trackguard.dev/track.js", data: { api_key: Trackguard.hub_api_key })
+    end
+
     def trackguard_meta_tags
       safe_join([
                   tag.meta(name: "trackguard-url", content: trackguard.page_views_path),
